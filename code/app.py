40c2921166f72652a77d37565a91cb7aa83571c0
Peng <pp2moonbird@gmail.com>
2016-11-06 22:24:52 +0800

    add post test

--- code/app.py
+++ code/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, jsonify
+import json
+
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,9 +13,21 @@ def index():
 
 @app.route('/data')
 def data():
-    student = {'name':'link', 'age':12}
+    result = None
+    with open('data.json', 'r') as inputfile:
+        student = json.load(inputfile)
     result = jsonify(student)
     return result
+
+
+@app.route('/data', methods=['POST'])
+def writeData():
+    a = request.json
+    print(a)
+    with open('data.json', 'w') as outputfile:
+        json.dump(a, outputfile)
+    return 'ok'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- code/static/js/tt.js
+++ code/static/js/tt.js
@@ -8,6 +8,12 @@ var app = new Vue({
     methods: {
         refresh: function(){
             this.$http.get('/data').then((response)=>{this.testText = response.body.name;});
+        },
+
+        postData: function(){
+            newName = this.testText;
+            resultObject = {'name': newName, 'age': 23};
+            this.$http.post('/data', resultObject).then();
         }
     }
 })
